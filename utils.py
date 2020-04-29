309e8f839d7f5037a84b98255a5f22428f308729
gregoirebailly <gregoire.bailly@insa-lyon.fr>
2020-04-29 15:45:05 +0200

    adding is valid to scoring function

--- utils.py
+++ utils.py
@@ -1,9 +1,13 @@
 from Person import *
 
-def score(population):
+def score(solution, quotas):
+    #check if the solution is valid
+    if isValid(solution, quotas) == False:
+        return float("inf")
+        
     Mtransports = dict()
     Etransports = dict()
-    for person in population:
+    for person in solution:
         #add 1 to the transports used on the morning
         Moption = person.morning[person.optionM]
         
@@ -33,3 +37,19 @@ def score(population):
         score =+ passengerNb * (passengerNb - 1)
 
     return score
+
+def isValid(solution, quotas):
+    workersByQuota = []
+    for i in quotas:
+        workersByQuota.append(0)
+
+    for person in solution:
+        if person.optionM != -1:
+            workersByQuota[person.domain] += 1
+    
+    for domainQuota, workers in quotas, workersByQuota:
+        if workers < domainQuota:
+            return False
+
+    return True
+
